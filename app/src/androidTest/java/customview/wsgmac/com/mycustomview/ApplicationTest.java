8c15e5db79f3a5c355e4f38fdc1f0e357c00f6d2
wushaoge <529500098@qq.com>
2015-11-07 01:34:31 +0800

    2333

--- app/src/androidTest/java/customview/wsgmac/com/mycustomview/ApplicationTest.java
+++ app/src/androidTest/java/customview/wsgmac/com/mycustomview/ApplicationTest.java
@@ -7,7 +7,7 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
-    //测试上船
+    //测试上船 日他哥 hahahh
     public ApplicationTest() {
         super(Application.class);
     }
